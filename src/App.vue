fe9b63a06727eac816dda7ff591b0e4f0b346fc9
dev-salman <isalman.developer@gmail.com>
2024-02-04 03:04:40 +0500

    last commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <NavbarVue />
+  <router-view />
+</template>
+
+<script>
+import NavbarVue from "./components/Navbar.vue";
+export default {
+  components: { NavbarVue },
+  data() {
+    return {};
+  },
+};
+</script>
